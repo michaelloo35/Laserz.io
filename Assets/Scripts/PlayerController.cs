20734557d03bef08e666bc0cf9185b91ddbd3dbd
michaelloo35 <michaelloo35@gmail.com>
2019-03-16 19:00:30 +0100

    2 Independent players with 2 xbox controllers. + Moving + Aiming

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    [FormerlySerializedAs("playerName")] public int playerId;
+    public GameObject weapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var inputXAxisMovement = Input.GetAxis($"Horizontal_{playerId}");
+        var inputYAxisMovement = Input.GetAxis($"Vertical_{playerId}");
+ 
+        MoveXY(inputXAxisMovement, inputYAxisMovement);
+
+        var aimXAxisDirection = Input.GetAxis($"Aim_Horizontal_{playerId}");
+        var aimYAxisDirection = Input.GetAxis($"Aim_Vertical_{playerId}");
+        AimWeapon(aimXAxisDirection, aimYAxisDirection);
+    }
+
+    private void MoveXY(float inputXAxis, float inputYAxis)
+    {
+        Vector3 moveVector = new Vector3(inputXAxis, inputYAxis, 0.0f);
+
+        transform.position += moveVector * Time.deltaTime;
+    }
+
+    private void AimWeapon(float aimXAxis, float aimYAxis)
+    {
+        if (Math.Abs(aimXAxis) > 0.01f && Math.Abs(aimYAxis) > 0.01f)
+        {
+            Vector3 aim = new Vector3(aimXAxis, -aimYAxis, 0.0f);
+            if (aim.magnitude > 0.0f)
+                aim.Normalize();
+            aim = aim * 0.5f;
+            weapon.transform.localPosition = aim;
+            weapon.transform.localRotation =
+                Quaternion.Euler(new Vector3(0.0f, 0.0f, 180 + Mathf.Rad2Deg * Mathf.Atan2(aimXAxis, aimYAxis)));
+        }
+    }
+}
