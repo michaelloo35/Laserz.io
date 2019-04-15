eb6c3cb5c8efbb654b8c27811e1b7422ed9e4180
michaelloo35 <michaelloo35@gmail.com>
2019-04-15 22:25:59 +0200

    Dagmara (#1)
    * add menu

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Menu
+{
+    public class Menu : MonoBehaviour
+    {
+        public Button playButton;
+        public Button quitButton;
+
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
