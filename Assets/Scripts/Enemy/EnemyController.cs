7aa75c0fea406a0f9d71ab1ddc428144ca678e09
Anna <anna.k.marciniec@gmail.com>
2019-05-26 11:10:20 +0200

    Enemies (#4)
    * Add enemy that follows player and shoots fire
    
    * Follow the closest player at given time
    
    * Damage player when collide with it
    
    * Changes after review.

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,74 @@
+﻿using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class EnemyController : MonoBehaviour
+{
+    public int id;
+    public EnemyStatus enemyStatus;
+
+    public float baseSpeed =0.8f;
+    public float stoppingDistance;
+
+    public Transform player;
+
+    public void Initialize(int enemyId)
+    {
+        id = enemyId;
+        enemyStatus = new EnemyStatus();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindClosestPlayer().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = FindClosestPlayer().transform;
+        float distance = Vector2.Distance(transform.position, player.position);
+
+        float speed = speedFunction(distance);
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerController>().TakeDamage(15);
+        }
+    }
+
+
+    public GameObject FindClosestPlayer()
+    {
+        GameObject[] gameObjects;
+        gameObjects = GameObject.FindGameObjectsWithTag("Player");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gameObjects)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+
+        return closest;
+    }
+
+    private float speedFunction(float distance)
+    {
+        return (float) (1 / (distance + baseSpeed) + 0.1);
+    }
+}
--- Assets/Scripts/PlayerSpawner/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner/PlayerSpawner.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 public class PlayerSpawner : MonoBehaviour
 {
     public PlayerController playerPrefab;
+    public EnemyController enemyPrefab;
     private Transform[] players;
+    private Transform[] enemies;
     private readonly int playerCount = 2;
+    private readonly int enemyCount = 2;
+
 
     // Start is called before the first frame update
     void Start()
     {
         players = new Transform[playerCount];
+        enemies = new Transform[enemyCount];
         SpawnPlayers(playerCount);
+        SpawnEnemies(enemyCount);
         InitializeCameraManager();
     }
 
@@ -24,8 +30,20 @@ public class PlayerSpawner : MonoBehaviour
             players[id] = player.transform;
         }
     }
+
+    private void SpawnEnemies(int numberOfEnemies)
+    {
+        for (int id = 0; id < numberOfEnemies; id++)
+        {
+            var enemy = Instantiate(enemyPrefab, new Vector3(1, id), Quaternion.identity);
+            enemy.Initialize(id);
+            enemies[id] = enemy.transform;
+        }
+    }
+    
     private void InitializeCameraManager()
     {
         GetComponent<CameraManager>().Initialize(players);
     }
+    
 }
